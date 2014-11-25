0e1f9f41fb229eefcb5f046fb6f8ff996deba0b7
Jindrich Pilar <pilar.jindrich@gmail.com>
2014-11-25 03:00:46 -0500

    New bundle

---
+++ Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Jinora\SkautisBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('SkautisBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
