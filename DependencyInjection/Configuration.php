67a0ea2289f69ffaa545c15bb93515e87ba00353
Jindrich Pilar <pilar.jindrich@gmail.com>
2014-11-27 08:03:11 -0500

    Namespace changed

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Jinora\SkautisBundle\DependencyInjection;
+namespace SkautisBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
